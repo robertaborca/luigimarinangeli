bbc53ef9f9fd72b14a19bee0c27fd475d6a0148c
Roberta Borca <robertaborca@gmail.com>
2024-10-27 16:20:01 +0100

    TEST UPLOAD

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,5 +1,7 @@
 const hamburger= document.getElementById("hamburger");
 const navigationBar = document.getElementById("navBar");
+const navLogo1 = document.getElementById("logo-1");
+const navLogo2 = document.getElementById("logo-2");
 const link1= document.getElementById("link-1");
 const link2= document.getElementById("link-2");
 const link3= document.getElementById("link-3");
@@ -8,6 +10,8 @@ const link3= document.getElementById("link-3");
 
 function navBar() {
       navigationBar.classList.toggle("responsive");
+      navLogo1.classList.toggle("hide");
+      navLogo2.classList.toggle("hide");
 }
 
 function links() {
