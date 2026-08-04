d841875255b93453e3ab957fe24abe99983d9d9c
Roberta Borca <robertaborca@gmail.com>
2026-08-04 19:07:22 +0200

    ok

--- scripts/nav.js
+++ scripts/nav.js
@@ -3,6 +3,7 @@ const navigationBar = document.getElementById("navBar");
 const link1= document.getElementById("link-1");
 const link2= document.getElementById("link-2");
 const link3= document.getElementById("link-3");
+const link4= document.getElementById("link-4");
 
 
 
@@ -22,6 +23,7 @@ function links() {
 link1.addEventListener("click", links);
 link2.addEventListener("click", links);
 link3.addEventListener("click", links);
+link4.addEventListener("click", links);
 
 
 hamburger.addEventListener("click", navBar); 
